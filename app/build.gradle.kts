277359bf1b589b3b166283cd0084e7d2c5377467
sh7verma <shubham.verma740@gmail.com>
2025-06-08 14:22:28 +0530

    initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.shverma.androidstarter"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.shverma.androidstarter"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
